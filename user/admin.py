2d73f98f6d00a5f6581f62abc4c92de02e8ade04
Quang Hoang <hoang.nqh207605@sis.hust.edu.vn>
2024-07-31 15:43:26 +0700

    test webhook

--- user/admin.py
+++ user/admin.py
@@ -7,3 +7,4 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user_name','address', 'phone','city','country','image_tag']
 
 admin.site.register(UserProfile,UserProfileAdmin)
+#this is test webhook
